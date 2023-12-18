ec0664488c5fd696404a105c51ced4a9a6504cbd
Dan Greaves <dan@dangreaves.com>
2023-12-18 12:03:25 +1100

    feat: wrap schema fields, validate file prior to conversion

--- src/transformers/schema.ts
+++ src/transformers/schema.ts
@@ -1,12 +1,15 @@
-import type { Transform } from "jscodeshift";
+import type { Transform, Collection, JSCodeshift } from "jscodeshift";
 
 const transformer: Transform = (fileInfo, api) => {
   const j = api.jscodeshift;
 
   const root = j(fileInfo.source);
 
+  // Check is valid for conversion.
+  if (!isRawSchemaFile(root, j)) return;
+
   // Resolve program.
-  const programPath = root.find(api.jscodeshift.Program).paths()[0];
+  const programPath = root.find(j.Program).paths()[0];
 
   // No program found.
   if (!programPath) return;
@@ -32,28 +35,52 @@ const transformer: Transform = (fileInfo, api) => {
   // Insert import node at top of program.
   programPath.node.body.unshift(sanityImportNode);
 
-  // Resolve schema.
-  const schemaPath = root
-    .find(api.jscodeshift.ObjectExpression)
-    .filter((path) => "ExportDefaultDeclaration" === path.parent.value.type)
-    .paths()[0];
+  // Wrap object expressions with sanity imports.
+  root.find(j.ObjectExpression).forEach((path) => {
+    // Root schema should be wrapped with defineType.
+    if ("ExportDefaultDeclaration" === path.parent.value.type) {
+      path.replace(
+        j.callExpression(
+          {
+            type: "Identifier",
+            name: "defineType",
+          },
+          [path.node],
+        ),
+      );
 
-  // No schema found.
-  if (!schemaPath) return;
+      return;
+    }
 
-  // Create wrapped schema.
-  const wrappedSchemaNode = j.callExpression(
-    {
-      type: "Identifier",
-      name: "defineType",
-    },
-    [schemaPath.node],
-  );
-
-  // Replace schema with wrapped schema.
-  schemaPath.replace(wrappedSchemaNode);
+    // Field objects should be wrapped with defineField.
+    if (
+      "ArrayExpression" === path.parent.value.type &&
+      "Property" === path.parent.parent.value.type &&
+      "fields" === path.parent.parent.value.key.name
+    ) {
+      path.replace(
+        j.callExpression(
+          {
+            type: "Identifier",
+            name: "defineField",
+          },
+          [path.node],
+        ),
+      );
+    }
+  });
 
   return root.toSource();
 };
 
 export default transformer;
+
+/**
+ * Return true if the given root has an unwrapped object export.
+ */
+function isRawSchemaFile(root: Collection, j: JSCodeshift) {
+  return !!root
+    .find(j.ObjectExpression)
+    .filter((path) => "ExportDefaultDeclaration" === path.parent.value.type)
+    .length;
+}
