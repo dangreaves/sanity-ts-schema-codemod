4ab0e02bb8f63eff4f8a6599573845ac8cbf90e6
Dan Greaves <dan@dangreaves.com>
2023-12-18 11:17:32 +1100

    feat: call transformer in convert-schemas command

--- src/commands/convert-schemas.ts
+++ src/commands/convert-schemas.ts
@@ -1,9 +1,20 @@
-export function convertSchemas({
+import path from "node:path";
+
+import { globby } from "globby";
+import { run as jscodeshift } from "jscodeshift/src/Runner.js";
+
+const transformPath = path.resolve("src/transformers/schema.ts");
+
+export async function convertSchemas({
   input,
   output,
 }: {
   input: string;
   output: string;
 }) {
-  console.log({ input, output });
+  const jsPaths = await globby(path.join(input, "**/*.js"));
+
+  await jscodeshift(transformPath, jsPaths, {
+    verbose: true,
+  });
 }
